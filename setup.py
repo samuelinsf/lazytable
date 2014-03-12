0351f167a6c163178efee871a223d449e6c92504
samuelinsf <sam-mail-1@tandu.com>
2014-03-11 23:23:54 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='lazytable',
+    version='0.1dev',
+    packages=['lazytable',],
+    url='https://github.com/samuelinsf/lazytable',
+    license='Software license AGPL version 3.',
+    author='samuelinsf',
+)
